6f7e36a6dd50734b820efaf5cfb3bf05cb497a46
Federico Lombardi <federicolombardi_10@hotmail.com>
2022-06-27 16:37:27 -0300

    navbar

--- src/components/Navbar/NavBar.js
+++ src/components/Navbar/NavBar.js
@@ -2,30 +2,25 @@ import * as React from 'react';
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
-import IconButton from '@mui/material/IconButton';
-import MenuIcon from '@mui/icons-material/Menu';
+import './NavBar.scss'
 
 const NavBar=()=>{
     return(
         <Box sx={{ flexGrow: 1 }}>
-            <AppBar position="static">
-            <Toolbar>
-                <IconButton
-                size="large"
-                edge="start"
-                color="inherit"
-                aria-label="menu"
-                sx={{ mr: 2 }}
-                >
-                <MenuIcon />
-                </IconButton>
-                <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-                News
-                </Typography>
-                <Button color="inherit">Login</Button>
-            </Toolbar>
+            <AppBar position="static" className='navBar'>
+                <div className='divLogo'>
+                    <img src='./logoFedePrueba.svg' alt='imageLogo' className='imageLogo'/>
+                </div>
+                <div className='divMenu'>
+                    <ul>
+                        <li>Inicio</li>
+                        <li>Productos</li>
+                        <li>Ayuda</li>
+                        <li>Contacto</li>
+                    </ul>
+                </div>
+                <div className='divCartLogin'>Login</div>
             </AppBar>
         </Box>
     )
